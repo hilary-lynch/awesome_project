d209c371db98b0885fe3a0963f824745448e71cf
HilaryLynch <hlynch3@gmail.com>
2015-10-14 20:02:42 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+#the above shows that the WelcomeController inherits from ApplicationController
+
+#called an action. within the WeclcomeController
+  def coolpage
+  end
+
+end
